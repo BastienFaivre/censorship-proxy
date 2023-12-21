60ad9e8b518c0eab3004e67869fe18293b215964
Bastien Faivre <faivre.bast@gmail.com>
2023-12-21 13:33:59 +0100

    feat: more logs

--- connection/clientConnection.go
+++ connection/clientConnection.go
@@ -121,6 +121,8 @@ func proxyClientToTarget(closeChannel chan bool, clientConn net.Conn, targetConn
 				clientLoggers.Error.Println("Error writing request:", err)
 				break
 			}
+		} else {
+			clientLoggers.Warning.Println("Censored request:", request)
 		}
 	}
 	clientLoggers.Info.Println("ClientToTarget closed for client", clientConn.RemoteAddr())
